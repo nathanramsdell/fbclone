2a5ccc72729b9efe080c4c8e6c7195b5815aa2d9
Nathan Ramsdell <nathanramsdell@Nathans-MBP.hsd1.mn.comcast.net>
2021-07-13 13:49:48 -0500

    fixed facebook login

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Login from "../components/Login"
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
-        <title>Facebook</title>
+        <title>Natebook</title>
       </Head>
 
         {/* HEADER */}
@@ -18,3 +22,14 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  //Get the user
+  const session = await getSession(context);
+
+    return{
+      props: {
+        session
+      }
+    }
+  }
