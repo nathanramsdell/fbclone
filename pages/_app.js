d9ecd89a350bc44f36f45e0c57b78e30447015e3
Nathan Ramsdell <nathanramsdell@Nathans-MBP.hsd1.mn.comcast.net>
2021-07-13 09:07:54 -0500

    fixed things in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
+
 import { Provider } from "next-auth/client";
 
-function MyApp({ Component, pageProps }) {
-  return {
+import { Provider } from 'next-auth/client'
+
+export default function App ({ Component, pageProps }) {
+  return (
     <Provider session={pageProps.session}>
-    <Component {...pageProps} />
+      <Component {...pageProps} />
     </Provider>
-  }
+  )
 }
 
 export default MyApp
